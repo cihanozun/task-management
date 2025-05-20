f97c09343a24f77005d9df517d706169f6226543
cihanozun <cihanozun@gmail.com>
2025-05-21 01:11:25 +0300

    task management system

--- src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../../services/employee.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +8,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  
+  listofTasks: any[] = [];
+
+  constructor(private employeeService: EmployeeService,
+              private snackBar: MatSnackBar,
+  ) {
+
+    this.getTasks();
+  }
+
+  getTasks(){
+    this.employeeService.getEmployeeTasksById().subscribe(res => {
+      this.listofTasks = res;
+      console.log(this.listofTasks);
+    })
+  }
+
+  updateStatus(id:number, status:string){
+    this.employeeService.updateStatus(id,status).subscribe(res => {
+      if(res.id != null){
+        this.snackBar.open("Status Updated Successfully", "OK", {
+          duration: 2000,
+          verticalPosition: 'top',
+          horizontalPosition: 'right'
+        });
+        this.getTasks();
+      }else{
+        this.snackBar.open("Error in Updating Status", "OK", {
+          duration: 2000,
+          verticalPosition: 'top',
+          horizontalPosition: 'right'
+        });
+      }
+    })
+  }
+
 
 }
